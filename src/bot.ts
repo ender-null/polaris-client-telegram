238fd042eaf02bf9a735f817c17a97a10f18cc21
ender <ender@end.works>
2024-04-12 04:44:49 +0200

    fix bug when missing captions

--- src/bot.ts
+++ src/bot.ts
@@ -168,7 +168,7 @@ export class Bot {
     if (msg.extra && msg.extra.format && msg.extra.format === 'HTML') {
       caption = htmlToMarkdown(caption);
     }
-    caption = caption.trim()
+    caption = caption?.trim()
     if (msg.type == 'text') {
       if (!msg.content || (typeof msg.content == 'string' && msg.content.length == 0)) {
         return null;
